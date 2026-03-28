bf137c02f3693e8fb85f8c1f85a1507b7bbe2f72
Shivam <mypersonalishere.mypersonal@gmail.com>
2026-03-28 23:52:15 +0530

    Finished Async, Await & Fetch API via Md files

---
+++ front-end/05-async await & fetch API/Notes.md
@@ -0,0 +1,59 @@
+- Title: Re Promise with async await
+- dependent on file: yes
+- Other: Video no: 76
+```
+index.html
+async_await.js
+```
+
+---
+## Why using Async & Await 
+- To deal with asynchronous nature
+
+Asynchronous nature = Let task 'A: fast',  'B: slow'
+code is like: A1 B1 A2 A3
+want: A1 B1 A2 A3
+but executes like: A1 A2 A3 B1
+- Problem: if A2 is dependent on B1 (error)
+### Example
+- A1 returns a "pending promise" and js shows asynchronous nature.
+- A2 must not executes untill data is obtained.
+```
+console.log('Initializing');                          //A1
+function GetData(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log('Dowloading Data');
+        },3500)
+    })
+}
+let data=GetData();                                  //B1
+console.log(data);                                   //A2
+console.log("using data")                            //A3
+```
+
+### ? Await 
+without await we get a unfinished promise error (may)
+with await we get a resolved promise's Data (want)
+#### Solution 1 (Not good)
+- Using .then function   `L41 async_await.js`
+
+#### Solution 2 (Good)
+- Using Async & Await          L62  `without await L87 executes immediately `
+
+#### Solution 3 (Same as 2 + dummy API)
+- Using Async & Await          L101  `without await L102 & L108 executes in immediately`
+
+
+
+### Other 
+ - [Json Placeholder](https://jsonplaceholder.typicode.com/) Free dummy rest API
+ - Use: need two then ? ∵    1> gives data 2> parse data in json (takes some time) 
+ - json = JavaScript Object
+- Settled = Promise is either Resolved or Rejected
+- Resolve = only success
+- Resolve = only fail
+- Get Req : accessing a website
+- Post Req : Submitting a form
+- Put : Update
+- Delete 
--- front-end/05-async await & fetch API/async_await.js
+++ front-end/05-async await & fetch API/async_await.js
@@ -1,98 +1,96 @@
 // Using Data even before Dowloading results> painding Promise
 
-// console.log('Initializing');
-// console.log('Loading the data source');
+console.log('Initializing');
+console.log('Loading the data source');
 
-// function GetData(){
-//     return new Promise((resolve,reject)=>{
-//         setTimeout(()=>{
-//             console.log('Dowloading Data');
-//         },3500)
-//     }) 
-// }
+function GetData(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log('Dowloading Data');
+        },3500)
+    }) 
+}
 
-// let data=GetData();
-// console.log('above');
+let data=GetData();
+console.log('above');
 
-// console.log(data);
-// console.log('below');
+console.log(data);
+console.log('below');
 
-// console.log('Above promise is pending> the data will load after 3.5 seconds but the process who need that daat is already intiated');
+console.log('Above promise is pending> the data will load after 3.5 seconds but the process who need that data is already intiated');
 
-// console.log('Processing Data');
+console.log('Processing Data');
 
 // Using .then functionto wait untill Data loads to use
 
-// console.log('Initializing');
-// console.log('Loading the data source');
+console.log('Initializing');
+console.log('Loading the data source');
 
-// function getData() {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             console.log('Dowloading data');
+function getData2() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Dowloading data');
+            resolve(333);     //RESOLVE IS MOST IMPORTANT
+        }, 3500)
+    })
+}
 
-//             resolve(333);     //RESOLVE IS MOST IMPORTANT
-//         }, 3500)
-//     })
-// }
+let data2 = getData2();
+console.log('above');
 
-// let data = getData();
-// console.log('above');
-
-// data.then((V) => {
-//     console.log(data);
-//     console.log('Using .then fuction')
-//     console.log('below');
-
-//     console.log('Processing Data');
-// })           
+data.then((V) => {
+    console.log(data2);
+    console.log('Using .then fuction')
+    console.log('below');
+    console.log('Processing Data');
+})           
 
 // Using Async await to wait untill Data loads to use > ----asynch fuction start working from begining of execution
 
-// console.log('Initializing');
-// console.log('Loading the data source');
+console.log('Initializing');
+console.log('Loading the data source');
 
-// async function getData() {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             console.log('Dowloading data');
+async function getData() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Dowloading data');
 
-//             resolve(333);     //RESOLVE IS MOST IMPORTANT
-//         }, 3500)
-//     })
-// }
-// async function main() {
-//     let data = await getData();          //Await is used to stop below untill data is dowloading
-//     console.log('above');
-//     console.log(data);
-//     console.log('Using await fuction')
-//     console.log('below');
-//     console.log('Processing Data');          
-// }
+            resolve(333);     //RESOLVE IS MOST IMPORTANT
+        }, 3500)
+    })
+}
+async function main() {
+    let data = await getData();          //Await is used to stop below untill data is dowloading
+    console.log('above');
+    console.log(data);
+    console.log('Using await fuction')
+    console.log('below');
+    console.log('Processing Data');          
+}
 
-// main()
+main()
 
-//Using fetch API
+// Using fetch API
 
-// console.log('Initializing');
-// console.log('Loading the data source');
+console.log('Initializing');
+console.log('Loading the data source');
 
-// async function getData() {
-//     await fetch('https://jsonplaceholder.typicode.com/todos/1')
-//     .then(response => response.json())
-//     .then(json => console.log(json))
-//     return 666;
-// }
-// async function main() {
-//     let data = await getData();          //Await is used to stop below untill data is dowloading
-//     console.log('above');
-//     console.log(data);
-//     console.log('Using await fuction')
-//     console.log('below');
-//     console.log('Processing Data');          
-// }
+async function getData() {
+    await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(json => console.log(json))
+    return 666;
+}
+async function main() {
+    let data = await getData();          //Await is used to stop below untill data is dowloading
+    console.log('above');
+    console.log(data);
+    console.log('Using await fuction')
+    console.log('below');
+    console.log('Processing Data');          
+}
 
-// main()
+main()
 
 //Using fetch API  only change in async function
 
@@ -100,10 +98,9 @@ console.log('Initializing');
 console.log('Loading the data source');
 
 async function getData() {
-    let x =await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    let x = await fetch('https://jsonplaceholder.typicode.com/todos/1')
     console.log(x);
     // let data =await x.text()   //get data as a string
-
     return 666;
 }
 async function main() {
