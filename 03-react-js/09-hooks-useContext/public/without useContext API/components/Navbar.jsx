64c1b45e1cf604a60a101494984e8c6dcd65a714
Shivam <mypersonalishere.mypersonal@gmail.com>
2026-05-24 21:16:36 +0530

    Init: Initialize useMemo

---
+++ 03-react-js/09-hooks-useContext/public/without useContext API/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Button from "./Button.jsx"
+
+const Navbar = ({count}) => {
+  return (
+    <>
+
+    <div>
+      Navbar
+    </div>
+      <Button count={count} />
+    </>
+  )
+}
+
+export default Navbar
