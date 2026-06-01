01c9d040fe8676e0100f9f53c45d023133d74ee4
Shivam <mypersonalishere.mypersonal@gmail.com>
2026-06-01 23:55:06 +0530

    Feat: Image size reduction while maintaining quality via Image Component

--- 04-next-js/03-/my-app/next.config.mjs
+++ 04-next-js/03-/my-app/next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+    images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'example.com',
+        port: '',
+        pathname: '/account123/**',
+        search: '',
+      },
+    ],
+  },
   reactCompiler: true,
 };
 
