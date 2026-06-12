ebf82b46d94a831af272686645ce7bc7a0dc14d3
Shivam <mypersonalishere.mypersonal@gmail.com>
2026-06-12 22:00:25 +0530

    feat: Condition based slug route

--- 04-next-js/08-dynamic-Routes/my-app/01-README.md
+++ 04-next-js/08-dynamic-Routes/my-app/01-README.md
@@ -1,7 +1,15 @@
 description
 
 ---
+# Dynamic Routing
+
+- Example: 50 pages exist for blogposts
+- Via a single route we connect all
+
+---
 ## Slug routing Dynamic Route
+
+- 2nd Commit
 
 1. `Root/app/blog/page.js`
 2. `Root/app/blogpost/[slug]/page.js`
--- 04-next-js/08-dynamic-Routes/my-app/app/blogpost/[slug]/page.js
+++ 04-next-js/08-dynamic-Routes/my-app/app/blogpost/[slug]/page.js
@@ -1,5 +1,11 @@
 export default async function Page({ params }) {
-  const { slug } = await params
+  const { slug } = await params;
 
-  return <div>My Post: {slug}</div>
+  let languages = ["python", "cpp", "javascript", "dsa"];
+
+  if (languages.includes(slug)) {
+    return <div>My Post: {slug}</div>;
+  } else {
+    return <div>404: Does not exist</div>;
+  }
 }
