02169a8f20d55c8e4ef3ec6aba094350a7317d77
Shivam <mypersonalishere.mypersonal@gmail.com>
2026-06-12 13:08:18 +0530

    Feat: Git Sign in option added via authentication next.js

--- 04-next-js/07-next-Auth/my-app/app/layout.js
+++ 04-next-js/07-next-Auth/my-app/app/layout.js
@@ -1,5 +1,8 @@
+import SessionWrapper from "./Components/SessionWrapper";
+
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { imageOptimizer } from "next/dist/server/image-optimizer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +25,12 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+      <SessionWrapper>
+        {children}
+      </SessionWrapper>
+        </body>
     </html>
   );
 }
+ 
